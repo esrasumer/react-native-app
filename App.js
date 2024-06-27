518938897c53d55ad5e161626c6ee384d6df5950
Esra Sümer <esrasumer>
2024-06-27 11:14:18 +0300

    changed the style

--- App.js
+++ App.js
@@ -5,7 +5,15 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>esra</Text>
-      <StatusBar style="auto" />
+      <View style={[styles.box, styles.box1]}>
+        <Text style={styles.text}>react native</Text>
+      </View>
+      <View style={[styles.box, styles.box2]}>
+        <Text style={styles.text}>react</Text>
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.text}>react</Text>
+      </View>
     </View>
   );
 }
@@ -17,4 +25,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  box: {
+    width: 200,
+    height: 200,
+    backgroundColor: 'red',
+    marginBottom: 10,
+  },
+  box1: {
+    backgroundColor: 'blue',
+  },
+  box2: {
+    backgroundColor: '#afff'
+  },
+  text: {
+    color: 'white',
+    textAlign: 'center',
+    backgroundColor: 'gray',
+    fontSize: 30,
+  }
 });
