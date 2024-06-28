02fd6e8a8d9f1228701d85b29d88371860769fec
Esra Sümer <esrasumer>
2024-06-28 14:06:29 +0300

    changed the style

--- App.js
+++ App.js
@@ -4,15 +4,13 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>esra</Text>
-      <View style={[styles.box, styles.box1]}>
-        <Text style={styles.text}>react native</Text>
+      <View style={styles.box1}>
+        <Text>react native</Text>
       </View>
-      <View style={[styles.box, styles.box2]}>
-        <Text style={styles.text}>react</Text>
-      </View>
-      <View style={styles.box}>
-        <Text style={styles.text}>react</Text>
+      <View style={styles.box2}>
+        <View style={styles.child1}></View>
+        <View style={styles.child2}></View>
+        <View style={styles.child3}></View>
       </View>
     </View>
   );
@@ -21,26 +19,43 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  box: {
-    width: 200,
-    height: 200,
-    backgroundColor: 'red',
-    marginBottom: 10,
+    backgroundColor: 'purple',
+    flexDirection: "column",
   },
   box1: {
-    backgroundColor: 'blue',
+    flex: 1,
+    backgroundColor: 'pink',
+    alignItems: "center",
+    justifyContent: "center",
   },
   box2: {
-    backgroundColor: '#afff'
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "azure",
+    justifyContent: "space-between",
+    alignItems:"center",
   },
-  text: {
-    color: 'white',
-    textAlign: 'center',
-    backgroundColor: 'gray',
-    fontSize: 30,
+  // child4:{
+  //   flex:1,
+  //   backgroundColor:"red",
+  // },
+  // child5:{
+  //   flex:1,
+  //   backgroundColor:"bisque",
+  // },
+  child1: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'green',
+  },
+  child2: {
+    backgroundColor: 'yellow',
+    width: 100,
+    height: 100,
+  },
+  child3: {
+    backgroundColor: "blue",
+    width: 100,
+    height: 100,
   }
 });
